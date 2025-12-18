7c66975208f39b4e9fe90fd8d19d8cef51239dfb
basedfigure <basedfigure@gmail.com>
2025-12-18 11:04:48 +0200

    Bash script to push everything in my private repos, for multiple gits, so that it proceeds to skip unedited trees, and a toggle to auto open the resulting dump log as file, instead of in console

--- sh/git_auto_commit_desk.sh
+++ sh/git_auto_commit_desk.sh
@@ -17,9 +17,41 @@
 
 # Git commit the following repo's changes with a generic message, so it's fire and forget.
 #   Note to self:   keep this script in particular intentionally basic:
-GIT="/mnt/dump_dsk/CENTR/WRK/_lan/DESK"
 
-cd "$GIT" || exit 1
-git add .
-git commit -m "Work"
-git push
+
+LOG="/home/bf/Desktop/cron_git_auto_commits.txt"
+exec >>"$LOG" 2>&1
+
+TOG_SUCC=0
+
+
+open_file() {
+  [ -n "$DISPLAY" ] && kwrite "$LOG" &
+}
+
+failed() {
+  echo "Error: $1"
+  [ -n "$DISPLAY" ] && kwrite "$LOG" &
+  exit 1
+}
+
+git_run() {
+  local git="$1"
+  cd "$git" 		 || failed "cd $git"
+  git add .  		 || failed "add"
+
+  if git diff-index --quiet HEAD --; then
+    echo "No changes to commit in $git"
+  else
+    git commit -m "Work" || failed "commit"
+    git push 		 || failed "push"
+  fi
+}
+
+git_run "/mnt/dump_dsk/CENTR/WRK/_lan/DESK"
+git_run "/mnt/dump_dsk/CENTR/WRK/_lan/WSPC"
+
+
+if [ "$TOG_SUCC" -eq 1 ]; then
+  open_file
+fi
