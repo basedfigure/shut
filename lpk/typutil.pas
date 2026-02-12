5c05303c19aeb716fe5565ec6852a7a8010ac9ec
basedfigure <basedfigure@gmail.com>
2026-02-12 22:09:18 +0200

    Types, style guide for recognizing between a variable names that are either abbreviated or not

--- lpk/typutil.pas
+++ lpk/typutil.pas
@@ -12,39 +12,66 @@ const
   IO_PATH_R = '../../../io/';
   SH_PATH_R   = '../../../sh/';
 
-
-  // Consoles:
-  {
-  To change the console you want, hit the Run's arrow button, Run Parameters...
-  [x] Use launching application
-
-  TDE has options for Xterm and Konsole in the below dropdown box
-  }
-
-  // lib folder bugfix
-  {
-  if you get scoping bugs, after updating your lpk or unit paths, you should del
-  ete your lib folder for that project and recompile
-  }
-
-
 type
+  // Style guide (e.g):
+  // Abbreviate block to blk, when you don't mean a memory block (mem), but to m
+  // ean something more disambiguous without context, like a comment block (str)
+
   // Type aliasing
   int  = integer;
   str  = string;
   bool = boolean;
+  // ..arrays
+  str_a  = array of string;
+  var_a  = array of variant;
+  poi_a  = array of pointer;
+  // ..pointers
+  int_p  = PInteger;
+  str_p  = PString;
+  bool_p = PBoolean;
 
+  // Typed subroutines
+  proc_str_t = procedure (const arg:  str);
+
+
+  { (arg)ument }
+
+  arg_t = record
+    proc:  proc_str_t;
+    id:  str;
+  end;
+
+  arg_a = array of arg_t;
+
+
+var
+  // Global variables
+  pa:  poi_a;
+  // ..arrays
+  g_vars:  arg_a;
+
+  { (poi)nter }
+
+
+  { (de)bug }
   procedure de_print_ln (s: str = '');
-  procedure de_print_blk (s: str;  head: str = 'FILL ME';  sym: str = ':');
+  procedure de_print_blk (s: str;  head: str = 'FILL UP';  sym: str = ':');
   procedure de_print_env (user, term: str);
 implementation
+
+
+{ (poi)nter }
+
+
+
+{ (de)bug }
 
 procedure de_print_ln (s: str = '');
 begin
   Writeln (s);
 end;
 
-procedure de_print_blk (s: str;  head: str = 'FILL ME';  sym: str = ':');
+procedure de_print_blk (s: str;  head: str = 'FILL UP';  sym: str = ':');
 begin
   Writeln ('');
   Writeln (head + sym);
