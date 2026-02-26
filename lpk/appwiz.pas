f4b32695a4dc48c827b8b18726288005e74ec7fc
basedfigure <basedfigure@gmail.com>
2026-02-26 20:00:27 +0200

    dummies

--- lpk/appwiz.pas
+++ lpk/appwiz.pas
@@ -9,11 +9,28 @@ uses
   // Juju
   typutil;
 
-procedure inst_clone_dep (git:  str);
+// Tools:
+// ..git
+procedure inst_clone_dep_fr_git (u:  url_t);   // note:  _git instead git_
+procedure inst_new_ver_fr_git (u:  url_t);
+// ..firefox
+procedure open_url_in_fox (u:  url_t);
 
 implementation
 
-procedure inst_clone_dep (git:  str);
+
+
+procedure inst_clone_dep_fr_git (u:  url_t);
+begin
+
+end;
+
+procedure inst_new_ver_fr_git (u:  url_t);
+begin
+
+end;
+
+procedure open_url_in_fox (u: url_t);
 begin
 
 end;
