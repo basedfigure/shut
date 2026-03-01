3e330573cd9da59846236a98820c3fc7e80fa1c6
basedfigure <basedfigure@gmail.com>
2026-03-01 19:30:50 +0200

    Bash script, my gf is a good fit - or - how to enjoy the terminal, as a beginner and learn to just take it.  Bash works natively on Linux and MacOS, if you're into the black arts

--- sh/tde_startup.sh
+++ sh/tde_startup.sh
@@ -1,3 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+# Beginner tip:  if you're new to terminals, this is the best place to start usi
+# ng them. If you create just a couple of files with some content in the names a
+# nd a todo.txt with some content in it. If you are always pulling up your notes
+# then you want to automate it with just the 2 konsole commands you see below.
+#
+# Highly highly cursed:
+# My gf said she would like to try out black arts, but 1st she should understand
+# the implications of spending so much time in highly active environments like K
+# onsole. She's a big girl and she got in to the black arts, she likes The Hex.
+#
 
 # Startup applications:
+
+# Print diretory, where i keep some temp files with the todo's as the file names
+# - you can still back up this way by having them stored in git automatically li
+# ke here:
+#
+# https://github.com/basedfigure/juju/blob/main/sh/git_auto_commit_desk.sh
+#
+konsole -e bash -c "cd /mnt/dump_dsk/CENTR/WRK/_lan/DESK/hat && ls; exec bash" &
+
+
+# Open my todo list automatically upon startup. I call it with an alias, if it's
+# closed. Terminal tabs are also good if you lack the screen space (laptops).
+#
+konsole -e bash -c "vim /mnt/dump_dsk/CENTR/WRK/_lan/DESK/todo.txt"
