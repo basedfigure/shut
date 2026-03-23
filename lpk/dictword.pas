53bcb6b51505336ab2c6acda477014389c01dfdc
basedfigure <basedfigure@gmail.com>
2026-03-23 19:28:03 +0200

    init

---
+++ lpk/dictword.pas
@@ -0,0 +1,49 @@
+unit dictword;
+{ Structure this file or parse it directly to create your strings - includes the
+ladies, with and without pantaloons. }
+
+{ * Dictionary:
+
+* Words:  size, init
+A) able, ass
+B) bag, bane, bark, boon, bore, buff, buzz
+C) cat, core, cult, cold
+D) dash, damp, deck, dick, dig, dog, dull, duck, dump
+E) epic, echo
+F) fail, fuck
+G) gang, gat, gush
+H) hang, hash, help, hug, huge, hunk, hold, hunt
+I) info,
+J) jack,
+K) keep, kill,
+L) ling, low, lute
+M) merc, mast
+N) nerf,
+O) omit,
+P) pace, psy, push, poor
+Q) qi
+R) rest
+S) save, spur, sag
+T) tech, tit, tree
+U) urge, ulti
+V) vert
+W) wed
+X) XBox
+Y) yank, yes
+Z) zen
+
+* Antonyms:
+speed = slow/fast, stay/rush
+}
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
