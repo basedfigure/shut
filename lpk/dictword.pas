1860ee95d67f7e08b0c234ab7a56713ea769239a
basedfigure <basedfigure@gmail.com>
2026-03-24 15:12:14 +0200

    dict, misc

--- lpk/dictword.pas
+++ lpk/dictword.pas
@@ -2,35 +2,47 @@ unit dictword;
 { Structure this file or parse it directly to create your strings - includes the
 ladies, with and without pantaloons. }
 
-{ * Dictionary:
+{ * Dictionary:  dict
+
+do:  util to order by alphabet, copy/paste back, check for dupes
+- in console, no less
 
 * Words:  wide, init
-A) able, ass
-B) bag, bane, bark, boon, bore, buff, buzz
-C) cat, core, cult, cold
-D) dash, damp, deck, dick, dig, dog, dull, duck, dump
-E) epic, echo
-F) fail, fuck
-G) gang, gat, gush
-H) hang, hash, help, hug, huge, hunk, hold, hunt
+A) able, ass, aged
+B) bag, bane, bark, boom, boon, bore, buff, buzz, bug, borg, bork, baby, born
+   barf
+C) cam, cat, core, cult, cold, cage, chub
+D) dash, damp, demi, deck, dick, dig, dog, dull, duck, dump, dark, dork, dirk,
+   dirt
+E) echo, elf, epic, ever
+F) face, fad, fade, fail, fuck, flu, fat
+G) gang, gat, gush, glue
+H) hang, hash, head, help, hip, hug, huge, hunk, hold, hunt, hume, hip, hot, hid
+   hide
 I) info,
-J) jack,
+J) jack, jail
 K) keep, kill,
-L) ling, low, lute
-M) merc, mast
-N) nerf,
-O) omit,
-P) pace, psy, push, poor
+L) lass, leg, ling, live, lock, low, lute, lewd, lurk, lag, lick
+M) mast, merc, mix, mind, ms., mr.
+N) nerf, numb
+O) omit, open, off
+P) pace, pad, port, psy, push, poor, piss
 Q) qi
-R) rest
-S) save, spur, sag
-T) tech, tit, tree
+R) rest, rod, rock, rude, road
+S) sag, save, set, shut, spur, spy, star, sir, sire, shit, stab, sack, sake,
+   sage
+T) tape, tar, tase, task, tech, tit, tree, tire, tail, tush
 U) urge, ulti
 V) vert
-W) wed
+W) wed, wet, wire
 X) XBox
 Y) yank, yes
 Z) zen
+
+* Names:
+Kage
+
+Common glyphs & variable names:  do
 
 * Antonyms:
 speed = slow/fast, stay/rush
